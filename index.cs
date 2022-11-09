9125616ebea6944e223d2c8e67497880914aa9db
mahyar <100092924+mahyar-bInaRy@users.noreply.github.com>
2022-11-09 16:14:40 +0330

    Create index.cs

---
+++ index.cs
@@ -0,0 +1,29 @@
+using System;
+
+using System.Linq;
+
+using System.Collections.Generic;
+
+namespace HelloWold
+{
+    public static class Program 
+    {         
+        public static void Main()
+        {
+            int a;
+         for  (a=-999;a<=999;a++)
+Console.Write(a);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
