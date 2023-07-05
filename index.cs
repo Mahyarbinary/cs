ca1e08da93f473b387bac9393893196cfbad73d4
Mahyar binary <100092924+Mahyarbinary@users.noreply.github.com>
2023-07-06 00:04:38 +0330

    Update index.cs

--- index.cs
+++ index.cs
@@ -1,29 +1,3 @@
-using System;
-
-using System.Linq;
-
-using System.Collections.Generic;
-
-namespace HelloWold
 {
-    public static class Program 
-    {         
-        public static void Main()
-        {
-            int a;
-         for  (a=-999;a<=999;a++)
-Console.Write(a);
-        }
-    }
+  "message": "متن مورد نظر"
 }
-
-
-
-
-
-
-
-
-
-
-
